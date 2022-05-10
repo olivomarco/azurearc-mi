13fa6faddde0fffbc7a2e063b77aad3ec1881c28
Marco Olivo <8478776+olivomarco@users.noreply.github.com>
2022-05-10 18:23:22 +0200

    Restore code

---
+++ code/dotnet/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+
+Console.WriteLine("Trying to read a secret from a KeyVault using transparent authentication...");
+
+// Azure-Arc specific environment variables
+Environment.SetEnvironmentVariable("IDENTITY_ENDPOINT", "http://localhost:40342/metadata/identity/oauth2/token");
+Environment.SetEnvironmentVariable("IMDS_ENDPOINT", "http://localhost:40342");
+
+var client = new SecretClient(new Uri("https://passwordless-arc-mi.vault.azure.net/"), new DefaultAzureCredential());
+try
+{
+    KeyVaultSecret secret = await client.GetSecretAsync("my-onprem-secret");
+    Console.WriteLine(secret.Value);
+}
+catch (AuthenticationFailedException e)
+{
+    Console.WriteLine($"Authentication Failed. {e.Message}");
+}
